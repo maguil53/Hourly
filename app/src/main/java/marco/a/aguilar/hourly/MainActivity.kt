f8e0736a4c3a850ae0be909233807e9d8ac51587
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2020-09-21 22:37:19 -0700

    Almost done setting up ViewPager2 with TabLayout. So far it is able to display ProgressFragment and TasksFragment. I still need to figure out why clicking a tab doesn't do anything. Also, I need to add the icons to the tabs.

--- app/src/main/java/marco/a/aguilar/hourly/MainActivity.kt
+++ app/src/main/java/marco/a/aguilar/hourly/MainActivity.kt
@@ -1,12 +1,54 @@
 package marco.a.aguilar.hourly
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainFragmentAdapter: MainFragmentAdapter
+    private lateinit var viewPager: ViewPager2
+
+    companion object {
+        val fragmentList: ArrayList<Fragment> = arrayListOf(ProgressFragment(), TasksFragment())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainFragmentAdapter = MainFragmentAdapter(this)
+        viewPager = findViewById(R.id.pager)
+        viewPager.adapter = mainFragmentAdapter
+
+
+        // Attach tab_layout
+        /**
+         * Attaching TabLayout to ViewPager2. You have to set up the text
+         * and drawable here because the icon/text won't show up if you just
+         * put it in the XML file.
+         */
+        TabLayoutMediator(tab_layout, viewPager) { tab, position ->
+
+            when(position) {
+                0 -> {
+                    tab.text = "Progress"
+                    tab.icon = getDrawable(R.drawable.ic_launcher_background)
+                    viewPager.setCurrentItem(tab.position, true)
+                }
+
+                1 -> {
+                    tab.text = "Tasks"
+                    tab.icon = getDrawable(R.drawable.ic_launcher_background)
+                }
+            }
+
+
+
+        }.attach()
     }
 }
---
+++ app/src/main/java/marco/a/aguilar/hourly/MainFragmentAdapter.kt
@@ -0,0 +1,23 @@
+package marco.a.aguilar.hourly
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import marco.a.aguilar.hourly.MainActivity.Companion.fragmentList
+
+// FragmentStateAdapter takes in a fragment. So in order to
+// create to call MainFragmentAdapter(this), we should create it inside a
+// Fragment. Pretty sure we can do this inside an Activity too, but the Google
+// documentation did it from a Fragment.
+class MainFragmentAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+
+    override fun getItemCount(): Int = 2 // Only 2 Tabs
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+}
