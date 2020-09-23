798e85d9e2a69cec5749ccfc95f1d5880e073ae0
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2020-09-22 18:59:33 -0700

    Finished setting up TabLayout icons and text colors.

--- app/src/main/java/marco/a/aguilar/hourly/MainActivity.kt
+++ app/src/main/java/marco/a/aguilar/hourly/MainActivity.kt
@@ -1,11 +1,13 @@
 package marco.a.aguilar.hourly
 
 import android.content.ContentValues.TAG
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -32,22 +34,39 @@ class MainActivity : AppCompatActivity() {
          * and drawable here because the icon/text won't show up if you just
          * put it in the XML file.
          */
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.icon?.setTint(Color.parseColor("#D6D6D6"))
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.icon?.setTint(Color.parseColor("#838383"))
+            }
+        })
+
+
         TabLayoutMediator(tab_layout, viewPager) { tab, position ->
 
             when(position) {
                 0 -> {
                     tab.text = "Progress"
-                    tab.icon = getDrawable(R.drawable.ic_launcher_background)
+                    tab.icon = getDrawable(R.drawable.ic_progress)
+
                     viewPager.setCurrentItem(tab.position, true)
                 }
 
                 1 -> {
                     tab.text = "Tasks"
-                    tab.icon = getDrawable(R.drawable.ic_launcher_background)
+                    tab.icon = getDrawable(R.drawable.ic_tasks)
+                    // For initializing purposes only.
+                    tab.icon?.setTint(Color.parseColor("#838383"))
                 }
             }
-
-
 
         }.attach()
     }
