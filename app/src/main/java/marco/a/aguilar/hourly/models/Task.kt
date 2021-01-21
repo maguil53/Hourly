fa657400263296cfc6186dd2bbed121e88f1eca0
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2021-01-20 18:57:25 -0800

    Just added a comment to remind myself what I need to do in the future

--- app/src/main/java/marco/a/aguilar/hourly/models/Task.kt
+++ app/src/main/java/marco/a/aguilar/hourly/models/Task.kt
@@ -18,6 +18,25 @@ data class Task(
 
     companion object {
 
+        /**
+         * IMPORTANT:
+         *      We might need to refactor this elegant piece of code to include logic
+         *      related to the TaskType. Since we're going to include a button that lets
+         *      Users specify their sleep schedule (In which case TaskType will be RECOVER)
+         *      then we need to first check if type == TaskType.Recover. If it is Recover,
+         *      then this code should just evaluate the HourBlock as COMPLETE. Also, we want
+         *      the user to immediately see that their sleep schedule is in place (Green Squares).
+         *      So we're gonna have to change our logic in ProgressAdapter bc right now we're
+         *      only coloring the blocks of Hours that have passed, but we don't want this for the
+         *      Sleep Schedule...We might have to create a BlockType enums or use some easier
+         *      method to do this.
+         *
+         *      Later on we should provide a "Set Sleep Schedule from [] - []? Any Tasks that
+         *      are in these Hours will be deleted" Message before the User sets their sleep
+         *      schedule blocks...We could also have them delete it before doing that but
+         *      it would be too much of a hassle for them and it would require to much work
+         *      to check all the tasks in an HourBlock
+         */
         fun checkIfAllTasksAreComplete(tasks: List<Task>): Boolean {
             return tasks.all { it.isComplete }
         }
