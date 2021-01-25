b9be0b5c946f9eb6d2103190c8dc0c4af2bfb833
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2021-01-25 11:56:17 -0800

    Added code that removes the focus from an EditText if we click anywhere else on the screen, this way the keyboard will no longer show up. Also added a way to toggle between EditText and TextView so that when a user clicks on a description, they can start editing it. Still need to implement onBackPressed because there are some cases where the keyboard doesn't go away. There's also a small bug that keeps the keyboard open on the entire device if the app crashes or I think if we run the app again while we were already focused on the keyboard.

--- app/src/main/java/marco/a/aguilar/hourly/TaskCheckListActivity.kt
+++ app/src/main/java/marco/a/aguilar/hourly/TaskCheckListActivity.kt
@@ -1,22 +1,27 @@
 package marco.a.aguilar.hourly
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_task_check_list.*
-import kotlinx.android.synthetic.main.activity_task_check_list.my_toolbar
-import kotlinx.android.synthetic.main.toolbar_main.*
 import marco.a.aguilar.hourly.adapter.TaskCheckListAdapter
 import marco.a.aguilar.hourly.enums.TaskType
 import marco.a.aguilar.hourly.models.Task
 import marco.a.aguilar.hourly.models.TasksCompletedInfo
 
 
-class TaskCheckListActivity : AppCompatActivity() {
+class TaskCheckListActivity : AppCompatActivity(),
+    TaskCheckListAdapter.OnTaskTextViewClickedListener {
 
     private val TAG = "TaskCheckListActivity"
 
@@ -46,12 +51,9 @@ class TaskCheckListActivity : AppCompatActivity() {
          * to get rid of that too.
          */
         if(mTasksCompletedInfo != null) {
-            Log.d(TAG, "onCreate: tasksCompletedInfo: $mTasksCompletedInfo")
             // No way mTasksCompletedInfo is null here
             mCheckListTasks = (mTasksCompletedInfo!!.tasks)?.toMutableList() ?: mutableListOf<Task>()
         }
-
-        Log.d(TAG, "onCreate: ")
 
         setSupportActionBar(findViewById(R.id.my_toolbar))
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
@@ -69,6 +71,10 @@ class TaskCheckListActivity : AppCompatActivity() {
             } else {
                 mCheckListTasks.add(Task(TaskType.WORK, "", blockId!!, false))
                 viewAdapter.notifyDataSetChanged()
+
+                val testView = viewManager.findViewByPosition(mCheckListTasks.size - 1)
+                Log.d(TAG, "onCreate: " + testView)
+                Log.d(TAG, "onCreate: mCheckListTasks.size" + mCheckListTasks.size)
             }
         }
 
@@ -77,7 +83,7 @@ class TaskCheckListActivity : AppCompatActivity() {
 
     fun initRecyclerView() {
         viewManager = LinearLayoutManager(this)
-        viewAdapter = TaskCheckListAdapter(mCheckListTasks)
+        viewAdapter = TaskCheckListAdapter(mCheckListTasks, this)
 
         recyclerView = recycler_view_checklist.apply {
             /**
@@ -90,5 +96,68 @@ class TaskCheckListActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    /**
+     * Need to figure out how to find individual items
+     * inside the recyclerview so we can set a focus on a newly
+     * created item.
+     *
+     * Need to figure out how to put the Cursor all the way at the
+     * end
+     */
+    override fun onTaskTextViewClicked(textView: TextView, editText: EditText) {
+        editText.visibility = View.VISIBLE
+        textView.visibility = View.GONE
+
+        val stringValue = textView.text.toString()
+
+        /**
+         * setSelection() needs to come after requestFocus()
+         */
+        editText.requestFocus()
+        editText.setSelection(editText.text.length)
+        editText.append(stringValue)
+
+        /**
+         * This solved my issue! The keyboard now shows. Thanks to this article:
+         *  https://android.jlelse.eu/android-how-to-synchronize-keyboard-with-edittext-focus-8c1113797a15
+         */
+        val imm = editText.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+
+    /**
+     * Implement onBackPressed to hide the keyboard.
+     */
+
+    /**
+     * This code was taken from this post:
+     *  https://stackoverflow.com/questions/6677969/tap-outside-edittext-to-lose-focus
+     *
+     *  For some reason this was one of the few ways to make our EditText lose focus when clicking
+     *  outside of the EditText.
+     *
+     *  Note: We can also use this code for our Temi application!
+     */
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        if(event?.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+
+            if(v != null) {
+                if(v is EditText) {
+                    val outRect: Rect = Rect()
+                    v.getGlobalVisibleRect(outRect)
+
+                    if(!outRect.contains(event.getRawX().toInt(), event.getRawY().toInt())) {
+                        v.clearFocus()
+                        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(v.windowToken, 0)
+                    }
+                }
+            }
+        }
+
+        return super.dispatchTouchEvent(event)
     }
 }
--- app/src/main/java/marco/a/aguilar/hourly/adapter/TaskCheckListAdapter.kt
+++ app/src/main/java/marco/a/aguilar/hourly/adapter/TaskCheckListAdapter.kt
@@ -1,17 +1,27 @@
 package marco.a.aguilar.hourly.adapter
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.checklist_item.view.*
 import marco.a.aguilar.hourly.R
 import marco.a.aguilar.hourly.models.Task
 
-class TaskCheckListAdapter(private var mTasks: List<Task>)
+class TaskCheckListAdapter(private var mTasks: List<Task>,
+                           private var mOnTaskTextViewClickedListener: OnTaskTextViewClickedListener)
     : RecyclerView.Adapter<TaskCheckListAdapter.TaskCheckListViewHolder>() {
 
-    class TaskCheckListViewHolder(private val taskItem: View) : RecyclerView.ViewHolder(taskItem) {}
+    class TaskCheckListViewHolder(private val taskItem: View) : RecyclerView.ViewHolder(taskItem) {
+
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -28,15 +38,38 @@ class TaskCheckListAdapter(private var mTasks: List<Task>)
         holder: TaskCheckListViewHolder,
         position: Int
     ) {
-
         val taskDescription = mTasks[position].description
         val isComplete = mTasks[position].isComplete
 
         holder.itemView.textview_checklist_task_description.text = taskDescription
         holder.itemView.checkbox_checklist_task_iscomplete.isChecked = isComplete
 
+        val taskDescriptionTextView: TextView = holder.itemView.textview_checklist_task_description
+        val taskDescriptionEditText: EditText = holder.itemView.edittext_checklist_task_description
+
+        taskDescriptionTextView.setOnClickListener {
+            mOnTaskTextViewClickedListener.onTaskTextViewClicked(taskDescriptionTextView, taskDescriptionEditText)
+        }
+
+        taskDescriptionEditText.setOnFocusChangeListener { view, hasFocus ->
+            if(!hasFocus) {
+                val finalEditText = view as EditText
+                val finalString = finalEditText.text.toString()
+
+                taskDescriptionTextView.text = finalString
+
+                view.visibility = View.GONE
+                taskDescriptionTextView.visibility = View.VISIBLE
+                // Clear text or else it'll append the text twice.
+                view.text.clear()
+            }
+        }
 
     }
 
     override fun getItemCount() = mTasks.size
+
+    interface OnTaskTextViewClickedListener {
+        fun onTaskTextViewClicked(textView: TextView, editText: EditText)
+    }
 }
