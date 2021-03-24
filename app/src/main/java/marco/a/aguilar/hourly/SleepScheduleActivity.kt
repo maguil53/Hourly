b79750f8bfd1d04c744cd1a118c6e42f90b7e7ba
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2021-03-24 01:59:24 -0700

    Implemented the logic that will update our button_bedtime's text based on the Hour that the user selects

--- app/src/main/java/marco/a/aguilar/hourly/SleepScheduleActivity.kt
+++ app/src/main/java/marco/a/aguilar/hourly/SleepScheduleActivity.kt
@@ -2,6 +2,7 @@ package marco.a.aguilar.hourly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -12,6 +13,8 @@ import java.util.*
 class SleepScheduleActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
     lateinit var mTimePickerDialog: TimePickerDialog
+
+    private val TAG = "SleepScheduleActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +54,32 @@ class SleepScheduleActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetLis
         }
     }
 
+    /**
+     * Called whenever the user presses the "ok" button
+     *
+     * ToDo: Create a member variable that checks if both "bedtime" and "hours of sleep"
+     *  have been set. We should only write to our SharedPreferences once they are both set.
+     *  Not sure if we need to create an observer for both variables or how we're going
+     *  to implement it yet.
+     */
     override fun onTimeSet(view: TimePickerDialog?, hourOfDay: Int, minute: Int, second: Int) {
+        var timePeriod = ""
+        var newHour = hourOfDay
 
+        if (hourOfDay >= 12) {
+            if (hourOfDay != 12)
+                newHour = hourOfDay - 12
+
+            timePeriod = "PM"
+
+        } else {
+            if (hourOfDay == 0)
+                newHour = 12;
+
+            timePeriod = "AM";
+        }
+
+        val bedTimeString = "$newHour $timePeriod"
+        button_bedtime.text = bedTimeString
     }
 }
